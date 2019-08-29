308ce720e741bfec81c1d98d507f4e6d82cdf7f3
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-08-29 17:49:17 +0700

    add to cart 2

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,40 +1,39 @@
-import * as types from './../constants/ActionType'
+import * as Types from './../constants/ActionType'
 
 var data = JSON.parse(localStorage.getItem('CART'));
-var initialState = [
-    {
-        product: {
-            id: 1,
-            name: 'iPhone 7 Plus',
-            image: 'https://cdn.tgdd.vn/Products/Images/42/78124/iphone-7-plus-32gb-gold-600x600.jpg',
-            description: 'Sản phẩm do Apple Mỹ sản xuất',
-            price: 300,
-            inventory: 10,
-            rating: 4
-        },
-        quantity: 5
-    },
-    {
-        product: {
-            id: 3,
-            name: 'iPhone XS Max',
-            image: 'https://imgs.viettelstore.vn/images/Product/ProductImage/medium/Xs.jpg',
-            description: 'Sản phẩm do Apple Nhật Bản sản xuất',
-            price: 500,
-            inventory: 20,
-            rating: 4
-        },
-        quantity: 3
-    }
-];
+var initialState = data ? data : [];
 
 const cart = (state = initialState, action) => {
+    var { product, quantity } = action;
+    var index = -1;
     switch(action.type) {
-        case types.ADD_TO_CART:
-            console.log(action)
+        case Types.ADD_TO_CART:
+            index = findProductInCart(state, product);
+            if (index !== -1) {
+                state[index].quantity += quantity;
+            } else {
+                state.push({
+                    product,
+                    quantity
+                });
+            }
+            localStorage.setItem('CART'), JSON.stringify(state);
             return [...state]
         default: return [...state]
     }
+}
+
+var findProductInCart = (cart, product) => {
+    var index = -1;
+        if (cart.length > 0) {
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].product.id === product.id) {
+                    index = -1;
+                    break;
+                }
+            }
+        }
+    return index;
 }
 
 export default cart;
